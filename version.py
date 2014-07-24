73b0b8985712aa13c007ce0bf9b56fee79cee21d
Colin von Heuring <colin@janrain.com>
2014-07-23 21:51:56 -0700

    remove old documentation

--- version.py
+++ version.py
@@ -1,10 +1,1 @@
-"""
-Connect to the Toggl API.
-"""
-
-# This module is currently imported by ../setup.py
-# That may complicate things in the future if this module grows or begins to
-# import.  In such a case, consider creating a set-version script to handle
-# version updates.
-
 __version__ = '0.1.0-1.dev'
